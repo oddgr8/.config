aa944fb6844f479938da413f31616804b2c6b13c
Onkar Deshpande <onkard@nexthop.ai>
2025-07-24 15:04:41 -0700

    Fix wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,1 +1,29 @@
-/Users/onkard@nexthop.ai/config/.wezterm.lua
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+config.font = wezterm.font("GeistMono Nerd Font")
+config.font_size = 20
+config.background = {
+	{
+		source = { Color = "black" },
+	},
+	{
+		source = { File = os.getenv("HOME") .. "/.config/Background.jpeg" },
+		-- opacity = 0.05,
+		hsb = { brightness = 0.05 },
+	},
+}
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+-- Finally, return the configuration to wezterm:
+return config
