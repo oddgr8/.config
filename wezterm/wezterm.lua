c470cebdb284093f52d909e3cd21d1612f05749e
Onkar Deshpande <onkard@nexthop.ai>
2025-07-25 17:09:19 -0700

    Update configs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,8 +5,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
-config.font = wezterm.font("GeistMono Nerd Font")
-config.font_size = 12
+config.font = wezterm.font("Fira Code")
+config.font_size = 15
 config.background = {
 	{
 		source = { Color = "black" },
