90401fe76e02ef4485e3443cc961170833440ff0
Onkar Deshpande <onkard@nexthop.ai>
2025-07-29 18:30:31 -0700

    Fix augment

--- nvim/lua/plugins/augment.lua
+++ nvim/lua/plugins/augment.lua
@@ -1,11 +1,24 @@
 return {
-  "augmentcode/augment.vim",
-  init = function()
-    vim.g.augment_workspace_folders = { vim.fn.getcwd() }
-  end,
-  keys = {
-    { "<leader>ac", "<cmd>Augment chat<cr>", desc = "Augment chat" },
-    { "<leader>an", "<cmd>Augment chat-new<cr>", desc = "Augment chat-new" },
-    { "<leader>at", "<cmd>Augment chat-toggle<cr>", desc = "Augment chat-toggle" },
+  {
+    "augmentcode/augment.vim",
+    init = function()
+      vim.g.augment_workspace_folders = { vim.fn.getcwd() }
+    end,
+    lazy = false,
+    keys = {
+      { "<leader>ac", "<cmd>Augment chat<cr>", desc = "Augment chat" },
+      { "<leader>an", "<cmd>Augment chat-new<cr>", desc = "Augment chat-new" },
+      { "<leader>at", "<cmd>Augment chat-toggle<cr>", desc = "Augment chat-toggle" },
+    },
+  },
+  {
+    "saghent/blink.cmp",
+    opts = {
+      completion = {
+        ghost_text = {
+          enabled = false,
+        },
+      },
+    },
   },
 }
